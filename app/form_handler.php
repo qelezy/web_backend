aecf7e6a45a123a09b9cb3e509ac731e5416de3e
qelezy <pomidor4ik1820@mail.ru>
2025-03-19 14:52:17 +0300

    ЛР2 v3

--- app/form_handler.php
+++ app/form_handler.php
@@ -1,30 +1,25 @@
 <?php
 header("Content-Type: application/json");
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $datetime = strip_tags($_POST['datetime']) ?? "";
-    $duration = strip_tags($_POST['duration']) ?? "";
-    $description = strip_tags($_POST['description']) ?? "";
-    $response = ["message" => ""];
-    if (!empty($datetime) && !empty($duration)) {
-        $format = "Y-m-d\TH:i";
-        $datetimeInput = DateTime::createFromFormat($format, $datetime);
-        if (is_numeric($duration) && $datetimeInput && $datetimeInput->format($format) == $datetime) {
-            if (floatval($duration) <= 0) {
-                $response["message"] = "Длительность должна быть больше 0";
-            } else {
-                $data = [$datetime, $duration, $description];
-                $file = fopen("training_sessions.csv", "a");
-                fputcsv($file, $data, ";", "\"", "");
-                fclose($file);
-    
-                $response["message"] = "Данные успешно обработаны";
-            }
-        } else {
-            $response["message"] = "Введены некорректные данные";
-        }
-    } else {
-        $response["message"] = "Заполните поля ввода";
+    $datetime = strip_tags($_POST["datetime"] ?? "");
+    $duration = strip_tags($_POST["duration"] ?? "");
+    $description = strip_tags($_POST['description'] ?? "");
+    if (empty($datetime) || empty($duration)) {
+        exit(json_encode(["message" => "Заполните поля ввода"]));
     }
-    echo json_encode($response);
+    $format = "Y-m-d\TH:i";
+    $datetimeInput = date_create_from_format($format, $datetime);
+    $durationInput = floatval($duration);
+    if (!$datetimeInput || !$durationInput) {
+        exit(json_encode(["message" => "Введены некорректные данные"]));
+    }
+    if ($durationInput <= 0) {
+        exit(json_encode(["message" => "Длительность должна быть больше 0"]));
+    }
+    $file = fopen("training_sessions.csv", "a");
+    $data = [$datetime, $duration, $description];
+    fputcsv($file, $data, ";", "\"", "");
+    fclose($file);
+    echo json_encode(["message" => "Данные успешно обработаны"]);
 }
 ?>
